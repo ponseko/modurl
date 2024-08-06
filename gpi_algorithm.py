66e0a971aa926f5c5326344948c517ff4ed4669b
Koen Ponse <k.ponse@liacs.leidenuniv.nl>
2024-08-06 15:52:28 +0200

    training sa a classmethod of the gpi class and some refactoring

--- gpi_algorithm.py
+++ gpi_algorithm.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import jax
 import jax.numpy as jnp
 import numpy as np
@@ -6,17 +8,14 @@ import equinox as eqx
 from typing import Tuple
 import chex
 import gymnax
+from gymnax.environments.environment import Environment
 from functools import partial
 from env_wrappers import LogWrapper
 import flashbax
 from flashbax.buffers.trajectory_buffer import TrajectoryBufferState as BufferState, TrajectoryBuffer as Buffer
 from gpi_modules import *
 from util import log_callback, set_up_replay_buffer, set_up_optimizer, Transition, TrainBatch
-import navix
 from networks import ActorNetwork, ValueNetwork, Q_Network, Alpha
-from __future__ import annotations
-
-
 
 @chex.dataclass(frozen=True)
 class TrainState:
@@ -141,33 +140,53 @@ class GpiAlgorithm(eqx.Module):
     def load(self, path: str) -> GpiAlgorithm:
         return eqx.tree_deserialise_leaves(path)
     
-    def get_critic_output(self, critics: tuple, observation):
-        """ 
-            Get value from critic network, either a single critic or the minimum of two critics 
-            Provide target networks if required
+
+    @classmethod
+    def train(cls, rng: chex.PRNGKey, env: Environment, *, agent: GpiAlgorithm = None, hyperparams: GPI_hyperparams = None):
         """
-        for critic in critics:
-            assert isinstance(critic, eqx.Module)
-        if len(critics) == 1:
-            return jax.vmap(critics[0])(observation)
-        else: # dualing critic
-            return jnp.minimum(jax.vmap(critics[0])(observation), jax.vmap(critics[1])(observation))
+            This method trains an agent instance and returns the trained agent.
+            The number of training steps are determined by the provided hyperparameters.
+            if an agent object is provided, it will continue training from its checkpointed train state --
+            Creating a new agent will also provide an initial train state, so a new agent can be trained from scratch.
+            Alternatively, the train method may be provided an instance of hyperparemeters, in which case 
+            a new agent will be trained from scratch
 
-    def sample_data(self, runner_state):
+            @example use:
+                env = x.make("some_env")
+                agent = GPIAlgorithm.train(rng, env)
 
-        def _sample_step(runner_state, _) -> Tuple[Tuple, Transition]:
+            @alternative use:
+                env = x.make("some_env")
+                agent = GPIAlgorithm(rng, env)
+                agent = GPIAlgorithm.train(rng, agent, env)
+        """
+
+        def get_critic_output(critics: Tuple, observation):
+            """ 
+                Get value from critic network, either a single critic or the minimum of two critics 
+                Provide target networks if required
+            """
+            for critic in critics:
+                assert isinstance(critic, eqx.Module)
+            if len(critics) == 1:
+                return jax.vmap(critics[0])(observation)
+            else: # dualing critic
+                return jnp.minimum(jax.vmap(critics[0])(observation), jax.vmap(critics[1])(observation))
+            
+
+        def sample_data(runner_state, _) -> Tuple[Tuple, Transition]:
             train_state, env_state, last_obs, rng = runner_state
             rng, sample_key, step_key = jax.random.split(rng, 3)
 
             action_dist = jax.vmap(train_state.actor)(last_obs)
             action, log_prob = action_dist.sample_and_log_prob(seed=sample_key)
-            value = self.get_critic_output(train_state.critic, last_obs)
-            if self.hyperparams.use_Q_critic:
+            value = get_critic_output(train_state.critic, last_obs)
+            if hyperparams.use_Q_critic:
                 value = (value * action_dist.probs).sum(axis=-1)
 
             # take a step in the environment
             rng, key = jax.random.split(rng)
-            step_key = jax.random.split(key, self.hyperparams.num_envs)
+            step_key = jax.random.split(key, hyperparams.num_envs)
             obsv, env_state, reward, done, info = jax.vmap(
                 env.step, in_axes=(0, 0, 0, None)
             )(step_key, env_state, action, env_params)
@@ -186,106 +205,96 @@ class GpiAlgorithm(eqx.Module):
             runner_state = (train_state, env_state, obsv, rng)
             return runner_state, transition
         
-        """" First we do a rollout of `num_steps` steps in the environment """
-        (train_state, env_state, last_obs, rng), trajectory_batch = jax.lax.scan(
-            _sample_step, runner_state, None, self.hyperparams.num_steps
-        )
+        def process_sampled_data(train_state, trajectory_batch, last_obs, rng):
+            if hyperparams.use_Q_critic: # we need Q targets for the critic(s)
+                trajectory_batch_flat = jax.tree.map(lambda x: x.reshape((-1,) + x.shape[2:]), trajectory_batch)
+                next_action_dist = jax.vmap(train_state.actor)(trajectory_batch_flat.next_observation)
+                next_action_probs = next_action_dist.probs
+                next_action_probs_log = jnp.log(next_action_probs + 1e-8)
+                q_targets = get_critic_output(train_state.critic_target, trajectory_batch_flat.next_observation)
+                flat_targets = (next_action_probs * (q_targets - train_state.alpha() * next_action_probs_log)).sum(axis=-1)
+                flat_targets = trajectory_batch_flat.reward + ~trajectory_batch_flat.done * hyperparams.gamma * flat_targets
+                targets = flat_targets.reshape(trajectory_batch.reward.shape)
 
-        """ 
-            Then we calculate the training targets (advantages, returns, q_targets) 
-            This introduces some reduncancy as not all targets are used in all cases
-            For the time being we accept this small inefficiency for the sake of a simpler generalization 
-        """
-        if self.hyperparams.use_Q_critic: # we need Q targets for the critic(s)
-            trajectory_batch_flat = jax.tree.map(lambda x: x.reshape((-1,) + x.shape[2:]), trajectory_batch)
-            next_action_dist = jax.vmap(train_state.actor)(trajectory_batch_flat.next_observation)
-            next_action_probs = next_action_dist.probs
-            next_action_probs_log = jnp.log(next_action_probs + 1e-8)
-            q_targets = self.get_critic_output(train_state.critic_target, trajectory_batch_flat.next_observation)
-            flat_targets = (next_action_probs * (q_targets - train_state.alpha() * next_action_probs_log)).sum(axis=-1)
-            flat_targets = trajectory_batch_flat.reward + ~trajectory_batch_flat.done * self.hyperparams.gamma * flat_targets
-            targets = flat_targets.reshape(trajectory_batch.reward.shape)
+            last_value = get_critic_output(train_state.critic, last_obs)
+            if hyperparams.use_Q_critic:
+                action_dist = jax.vmap(train_state.actor)(last_obs)
+                last_value = (last_value * action_dist.probs).sum(axis=-1)
+            if hyperparams.use_gae: # use GAE
+                advantages, returns = calculate_gae_returns(trajectory_batch, last_value, hyperparams)
+            else:
+                advantages, returns = calculate_n_step_returns(trajectory_batch, last_value, hyperparams)
+            
+            train_batch = TrainBatch(
+                **trajectory_batch,
+                advantages=advantages,
+                returns=returns,
+                targets=targets
+            )
+            train_batch = jax.tree.map( # flatten over the envs
+                lambda x: x.reshape((-1,) + x.shape[2:]), train_batch
+            )
+            buffer_state = jax.jit(agent.buffer.add, donate_argnums=(0,))(train_state.buffer_state, train_batch)
+            train_state = eqx.tree_at(lambda x: x.buffer_state, train_state, buffer_state)
 
-        last_value = self.get_critic_output(train_state.critic, last_obs)
-        if self.hyperparams.use_Q_critic:
-            action_dist = jax.vmap(train_state.actor)(last_obs)
-            last_value = (last_value * action_dist.probs).sum(axis=-1)
-        if self.hyperparams.use_gae: # use GAE
-            advantages, returns = calculate_gae_returns(trajectory_batch, last_value, self.hyperparams)
-        else:
-            advantages, returns = calculate_n_step_returns(trajectory_batch, last_value, self.hyperparams)
-        
-        train_batch = TrainBatch(
-            **trajectory_batch,
-            advantages=advantages,
-            returns=returns,
-            targets=targets
-        )
-        train_batch = jax.tree.map( # flatten over the envs
-            lambda x: x.reshape((-1,) + x.shape[2:]), train_batch
-        )
-        buffer_state = jax.jit(self.buffer.add, donate_argnums=(0,))(train_state.buffer_state, train_batch)
-        train_state = eqx.tree_at(lambda x: x.buffer_state, train_state, buffer_state)
+            def create_minibatches(rng, _):
+                rng, shuffle_key, sample_key = jax.random.split(rng, 3)
+                train_batch = agent.buffer.sample(train_state.buffer_state, sample_key).experience.first
+                batch_size = train_batch.observation.shape[0]
 
-        runner_state = (train_state, env_state, last_obs, rng)
-        return runner_state, train_batch
-    
-    def preprocess_sampled_data(self, runner_state):
-        batch_size = int(self.hyperparams.num_envs * self.hyperparams.num_steps)
-        train_state, env_state, last_obs, rng = runner_state
-        rng, shuffle_key, sample_key = jax.random.split(rng, 3)
-        train_batch = self.buffer.sample(train_state.buffer_state, sample_key).experience.first
+                batch_idx = jax.random.permutation(shuffle_key, batch_size)
+                shuffled_batch = jax.tree.map(
+                    lambda x: jnp.take(x, batch_idx, axis=0), train_batch
+                )
+                minibatches = jax.tree.map( # If minibatches=False, num_minibatches=1
+                    lambda x: x.reshape((hyperparams.num_minibatches, -1) + x.shape[1:]), shuffled_batch
+                )
+                return rng, minibatches
 
-        batch_idx = jax.random.permutation(shuffle_key, batch_size)
-        shuffled_batch = jax.tree_util.tree_map(
-            lambda x: jnp.take(x, batch_idx, axis=0), train_batch
-        )
-        minibatches = jax.tree_util.tree_map( # If minibatches=False, num_minibatches=1
-            lambda x: x.reshape((self.hyperparams.num_minibatches, -1) + x.shape[1:]), shuffled_batch
-        )
-        # repeat the data for each update epoch #TODO: this is not consistent with actually doing multiple epochs, as in a normal sense we get different minibatches each time.
-        train_data = jax.tree_util.tree_map(
-            lambda x: jnp.repeat(x, self.hyperparams.update_epochs, axis=0), minibatches
-        )
+            rng, minibatched_train_data = jax.lax.scan(
+                create_minibatches, rng, None, hyperparams.update_epochs, unroll=True
+            )
+            train_batch = jax.tree.map( # flatten the epochs, so we have one single batch to scan over
+                lambda x: x.reshape((-1,) + x.shape[2:]), minibatched_train_data
+            )
 
-        return train_data
-    
-    def update_over_data(self, runner_state, train_batch: TrainBatch):
+            return train_state, train_batch, rng
 
-        def _update(runner_state, train_data: TrainBatch):
+        def update_over_data(update_state, train_data: TrainBatch):
 
-            def __policy_evaluation():
-                if self.hyperparams.use_Q_critic:
-                    critic_grads = q_loss(train_state.critic, self.hyperparams, train_data)
+            def _policy_evaluation():
+                if hyperparams.use_Q_critic:
+                    critic_grads = q_loss(train_state.critic, hyperparams, train_data)
                 else: # value network
-                    critic_grads = value_loss(train_state.critic, self.hyperparams, train_data)
-                updates, critic_opt_state = self.optimizer.update(critic_grads, train_state.critic_optimizer_state)
+                    critic_grads = value_loss(train_state.critic, hyperparams, train_data)
+                updates, critic_opt_state = agent.optimizer.update(critic_grads, train_state.critic_optimizer_state)
                 critic = optax.apply_updates(train_state.critic, updates)
                 return critic, critic_opt_state
             
-            def __policy_improvement():
-                critic_output = self.get_critic_output(train_state.critic, train_data.observation)
-                actor_grads = policy_loss(train_state.actor, self.hyperparams, train_data, critic_output, train_state_before_update.actor, train_state.alpha)
-                updates, actor_opt_state = self.optimizer.update(actor_grads, train_state.actor_optimizer_state)
+            def _policy_improvement():
+                critic_output = get_critic_output(train_state.critic, train_data.observation)
+                actor_grads = policy_loss(train_state.actor, hyperparams, train_data, critic_output, old_actor, train_state.alpha)
+                updates, actor_opt_state = agent.optimizer.update(actor_grads, train_state.actor_optimizer_state)
                 actor = optax.apply_updates(train_state.actor, updates)
                 return actor, actor_opt_state
             
-            train_state, env_state, last_obs, rng = runner_state
+            train_state, old_actor = update_state
+
+            updated_critic, updated_critic_opt_state = _policy_evaluation()
+            updated_actor, updated_actor_opt_state = _policy_improvement()
 
             # update target network
-            if self.hyperparams.use_target_networks:
-                critic_target = jax.tree.map(lambda x, y: y * 0.995 + x * 0.005, train_state.critic_target, train_state.critic)
+            if hyperparams.use_target_networks:
+                critic_target = jax.tree.map(lambda x, y: y * 0.9 + x * 0.10, train_state.critic_target, train_state.critic)
             else: critic_target = None
 
-            if self.hyperparams.learn_alpha:
+            if hyperparams.learn_alpha:
                 action_dist = jax.vmap(train_state.actor)(train_data.observation)
-                alpha_grads = alpha_loss(train_state.alpha, self.hyperparams, action_dist)
-                updates, updated_alpha_opt_state = self.optimizer.update(alpha_grads, train_state.alpha_optimizer_state)
+                alpha_grads = alpha_loss(train_state.alpha, hyperparams, action_dist)
+                updates, updated_alpha_opt_state = agent.optimizer.update(alpha_grads, train_state.alpha_optimizer_state)
                 updated_alpha = optax.apply_updates(train_state.alpha, updates)
             else: updated_alpha, updated_alpha_opt_state = train_state.alpha, train_state.alpha_optimizer_state
 
-            updated_critic, updated_critic_opt_state = __policy_evaluation()
-            updated_actor, updated_actor_opt_state = __policy_improvement()
             train_state = TrainState(
                 actor=updated_actor,
                 critic=updated_critic,
@@ -297,49 +306,58 @@ class GpiAlgorithm(eqx.Module):
                 critic_target=critic_target
             )
 
-            runner_state = (train_state, env_state, last_obs, rng)
+            update_state = (train_state, old_actor)
 
-            return runner_state, 0.0 # TODO: return loss info
-        
-        train_state_before_update, env_state, last_obs, rng = runner_state
+            return update_state, 0.0 # TODO: return loss info
 
-        runner_state, loss_info = jax.lax.scan(_update, runner_state, train_batch, unroll=4)
+        @partial(jax.jit, donate_argnums=(0,))
+        def train_step(runner_state, _):
+            
+            # sample
+            (train_state, env_state, last_obs, rng), trajectory_batch = jax.lax.scan(
+                sample_data, runner_state, None, hyperparams.num_steps
+            )
 
-        return runner_state, loss_info
+            # process
+            train_state, train_batch, rng = process_sampled_data(train_state, trajectory_batch, last_obs, rng)
 
-def train(rng: chex.PRNGKey, agent: GpiAlgorithm, env: gymnax.environments.environment.Environment):
+            # train
+            update_state = (train_state, train_state.actor)
+            update_state, loss_info = jax.lax.scan(
+                update_over_data, update_state, train_batch, unroll=4
+            )
 
-    @partial(jax.jit, donate_argnums=(0,))
-    def train_step(runner_state, _):
+            runner_state = (update_state[0], env_state, last_obs, rng)
 
-        runner_state, trajectory_batch = agent.sample_data(runner_state)
-        # train_batch = agent.get_critic_targets(runner_state, trajectory_batch)
-        train_batch = agent.preprocess_sampled_data(runner_state)
-        runner_state, loss_info = agent.update_over_data(runner_state, train_batch)
+            if hyperparams.debug:
+                metric = trajectory_batch.info
+                metric["loss"] = loss_info
+                metric["num_envs"] = agent.hyperparams.num_envs
+                jax.debug.callback(log_callback, metric)
 
-        if agent.hyperparams.debug:
-            metric = trajectory_batch.info
-            metric["loss"] = loss_info
-            metric["num_envs"] = agent.hyperparams.num_envs
-            jax.debug.callback(log_callback, metric)
+            return runner_state, trajectory_batch.info
 
-        return runner_state, trajectory_batch.info
+        if agent is None:
+            hyperparams = hyperparams or GPI_hyperparams()
+            agent = GpiAlgorithm(rng, env, hyperparams)
 
-    env_params = env.default_params
-    rng, reset_key = jax.random.split(rng)
-    reset_key = jax.random.split(reset_key, agent.hyperparams.num_envs)
-    obsv, env_state = jax.vmap(env.reset, in_axes=(0, None))(reset_key, env_params)
+        env_params = env.default_params
+        hyperparams = agent.hyperparams
+        batch_size = int(hyperparams.num_envs * hyperparams.num_steps)
+        num_iterations = int(hyperparams.total_timesteps // batch_size)
+        latest_or_new_train_state = agent.train_state_checkpoint
 
-    batch_size = int(agent.hyperparams.num_envs * agent.hyperparams.num_steps)
-    num_iterations = int(agent.hyperparams.total_timesteps // batch_size)
-    # minibatch_size = int(batch_size // agent.hyperparams.num_minibatches)
+        # reset the environment
+        rng, reset_key = jax.random.split(rng)
+        reset_key = jax.random.split(reset_key, hyperparams.num_envs)
+        obsv, env_state = jax.vmap(env.reset, in_axes=(0, None))(reset_key, env_params)
 
-    runner_state = (agent.train_state_checkpoint, env_state, obsv, rng)
-    runner_state, metrics = jax.lax.scan(
-        train_step, runner_state, None, num_iterations
-    )
+        runner_state = (latest_or_new_train_state, env_state, obsv, rng)
+        runner_state, metrics = jax.lax.scan(
+            train_step, runner_state, None, num_iterations
+        )
 
-    return agent, metrics
+        return agent, metrics
 
 
 if __name__ == "__main__":
@@ -348,4 +366,4 @@ if __name__ == "__main__":
     env = LogWrapper(env)
     key = jax.random.PRNGKey(0)
     agent = GpiAlgorithm(key, env)
-    train(key, agent, env)
+    GpiAlgorithm.train(key, env, agent=agent)
