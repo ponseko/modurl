e869124f3c5013bb149a2de95e4c1633a148993f
Koen Ponse <k.ponse@liacs.leidenuniv.nl>
2024-08-19 16:55:39 +0200

    sanity check changes

--- sb_sanity_check.py
+++ sb_sanity_check.py
@@ -6,18 +6,14 @@ from wandb.integration.sb3 import WandbCallback
 from stable_baselines3 import PPO, A2C
 from stable_baselines3.common.env_util import make_vec_env
 
-from gpi_algorithm import GpiAlgorithm, GpiHyperparams
-from env_wrappers import GymnaxWrapper, LogWrapper
-from default_params import DEFAULT_A2C_PARAMS, DEFAULT_PPO_PARAMS
-
 env_id = "CartPole-v1"
-
+env_ids = ["CartPole-v1", "Pendulum-v1", "Acrobot-v1" "MountainCar-v0", "bsuite/catch-v0"]
 seeds = np.arange(10).tolist()
 for seed in seeds:
     ## SB3
     config = {
         "policy_type": "MlpPolicy",
-        "total_timesteps": 500_000,
+        "total_timesteps": 1_000_000,
         "env_id": env_id,
     }
     wandb_run = wandb.init(project="sb3_sanity_check", config=config, sync_tensorboard=True)
@@ -40,28 +36,12 @@ for seed in seeds:
         callback=WandbCallback(verbose=2),
     )
     wandb_run.finish()
-
-
-    # GPI
-    env, env_params = gymnax.make(env_id)
-    env = GymnaxWrapper(env)
-    env = LogWrapper(env)
-    key = jax.random.PRNGKey(seed)
-
-    a2c_agent_params = GpiHyperparams(DEFAULT_A2C_PARAMS)
-    config = {"algo": "gpi_a2c", **a2c_agent_params.__dict__}
-    wandb_run = wandb.init(project="sb3_sanity_check", config=config)
-    
-    agent, metrics = GpiAlgorithm.train(key, env, hyperparams=a2c_agent_params)
-
-    wandb_run.finish()
-
     
 for seed in seeds:
     ## SB3
     config = {
         "policy_type": "MlpPolicy",
-        "total_timesteps": 500_000,
+        "total_timesteps": 1_000_000,
         "env_id": env_id,
     }
     wandb_run = wandb.init(project="sb3_sanity_check", config=config, sync_tensorboard=True)
@@ -70,8 +50,7 @@ for seed in seeds:
         config["policy_type"], 
         env, 
         stats_window_size=1, # No averaging 
-        verbose=1, 
-        use_rms_prop=False,
+        verbose=1,
         tensorboard_log=f"runs/{wandb_run.id}",
         policy_kwargs=dict(
             share_features_extractor=False,
@@ -83,19 +62,4 @@ for seed in seeds:
         total_timesteps=config["total_timesteps"],
         callback=WandbCallback(verbose=2),
     )
-    wandb_run.finish()
-
-
-    # GPI
-    env, env_params = gymnax.make(env_id)
-    env = GymnaxWrapper(env)
-    env = LogWrapper(env)
-    key = jax.random.PRNGKey(seed)
-
-    ppo_agent_params = GpiHyperparams(DEFAULT_PPO_PARAMS)
-    config = {"algo": "gpi_a2c", **ppo_agent_params.__dict__}
-    wandb_run = wandb.init(project="sb3_sanity_check", config=config)
-    
-    agent, metrics = GpiAlgorithm.train(key, env, hyperparams=ppo_agent_params)
-
     wandb_run.finish()
